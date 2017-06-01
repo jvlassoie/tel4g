c9f43a7cf18c9b10045422970701e38075252368
jean-valentin <jv.lassoie@gmail.com>
2017-06-01 19:43:47 +0200

    final form inscription

--- js/controller/registerCtrl.js
+++ js/controller/registerCtrl.js
@@ -1,10 +1,30 @@
-app.controller("registerCtrl", function ($scope,registerFactory,$stateParams,$window,$document,$q,lodash) {
+app.controller("registerCtrl", function ($scope,registerFactory,$stateParams,$window,$document,$q,lodash,$window) {
 /**************************************
 Ressources
 ***************************************/
 /* init var*/
 
-$scope.addUser = function(user){
+$scope.loader = false
+$scope.addUser = function(formUser){
+	$scope.loader = true
+	console.log(formUser)
+	var user = {
+		email: formUser.email,
+		firstname: formUser.firstName,
+		lastname: formUser.lastName,
+		birthdate: formUser.birthDate,
+		plainPassword: formUser.password
+	}
+	registerFactory.registerUser(user)
+	.then(function(msg){
+		$scope.loader = false
+		$window.alert("un nouvel inscrit !")
+		console.log("adding user successfully")
+		$window.location.href = '/index';
+	},function(msg){
+		console.log("failure")
+
+	})
 	console.log(user)
 }
 
--- js/factory/registerFactory.js
+++ js/factory/registerFactory.js
@@ -1,6 +1,13 @@
 app.factory('registerFactory', function($http,baseFactory){
 	var factory = {
-		
+		registerUser: function (user) {
+			return $http({
+			 method : "POST", 
+			 url : baseFactory.baseURL + "/users",
+			 data : user
+			})
+		}
+
 	}
 	return factory
 })
